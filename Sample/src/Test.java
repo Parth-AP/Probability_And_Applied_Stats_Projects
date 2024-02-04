495d8a0bcc3860196dc62526883bd210588521e1
pate4 <pate4@Party>
2024-02-03 20:36:12 -0500

    This is a test to see if this works and connect to github

---
+++ Sample/src/Test.java
@@ -0,0 +1,10 @@
+
+//This is a test to see if this project is able to connect to github
+
+public class Test {
+	
+	public static void main(String args[]) {
+		System.out.println("Hello World!!");	
+		}
+	
+}
